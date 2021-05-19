1f3659bcb3a995ee7038c60b592286ab2ef10763
JordanBlane <jordan26265071@live.com>
2021-05-19 20:59:26 +0100

    auto complete added

--- src/mainpage.js
+++ src/mainpage.js
@@ -12,13 +12,34 @@ export class MainPage extends React.Component {
         }
     }  
 
+    autocomplete = () => {
+        document.getElementById('dropdownDiv').innerHTML = '';
+        var postcode = document.getElementById('searchInput').value;
+        if(postcode == ''){return;}
+
+        fetch(`http://api.postcodes.io/postcodes/${postcode}/autocomplete`)
+        .then(result => result.json())
+        .then(data => {
+            if(!data.result){return;}
+            if(data.result.length < 1){return;}
+            for(let i = 0; i < data.result.length; i++)
+            {
+                let template = `<div onclick="window.location.href='http://${hostname}/postcode/${data.result[i]}'"> <h3>${data.result[i]}</h3> </div>`
+                document.getElementById('dropdownDiv').innerHTML += template;
+            }
+        })
+    }
+
 
     render(){
         return(
             <div className="MainPageContainer">
                 <div className="searchDiv">
-                    <input placeholder="Postcode.." className="mainpageInput" id='searchInput'/>
+                    <input placeholder="Postcode.." onChange={()=>{this.autocomplete()}} className="mainpageInput" id='searchInput'/>
                     <button className="mainpageButton" id='searchButton' onClick={()=>{window.location.href = `http://${hostname}/postcode/${document.getElementById('searchInput').value}`}}>🔍</button>
+                    <div className="dropdownDiv" id='dropdownDiv'>
+
+                    </div>
                 </div>
             </div>
         )
